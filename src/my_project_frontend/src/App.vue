766a55a2605ca3164580c13f295c3030b7a98f6b
pu0238 <pu0238@protonmail.com>
2024-11-27 10:15:28 +0000

    updated frontend

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let msg = ref('');
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+  const msgFromId = target.querySelector('#msg').value;
+  await my_project_backend.save_msg(msgFromId);
+  msg.value = await my_project_backend.get_msg();
 }
 </script>
 
@@ -19,10 +18,10 @@ async function handleSubmit(e) {
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
+      <label for="msg">Enter your msg: &nbsp;</label>
+      <input id="msg" alt="msg" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="msg">{{ msg }}</section>
   </main>
 </template>
